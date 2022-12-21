e1a6aec5ea9e3a966f6cbd4bc15cba1f53798140
Censuradho <gustavoleiteoliveira800@gmail.com>
2022-12-21 15:03:24 -0300

    feat(google analytics)

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ This is a blog that uses [Next.js](https://nextjs.org/) in the client, using the
 
 <img src="https://user-images.githubusercontent.com/49209628/208965185-c2c8270b-b3ef-42ef-89f9-7f59a08ca69d.png" alt="" width="500px" />
 <img src="https://user-images.githubusercontent.com/49209628/208965242-924d6cbd-7322-4c89-9983-b383ae705cce.png" alt="" width="300px" />
+
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import type { AppProps } from 'next/app'
+import reactGA from "react-ga4";
 
 import { ThemeProvider } from 'providers'
+import { useEffect } from 'react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    reactGA.initialize(process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS as string);
+  }, [])
+
   return (
     <ThemeProvider>
       <Component {...pageProps} />
