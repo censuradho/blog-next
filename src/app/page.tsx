aea221b9aeab3d90cf2a088db0c8414a57511b74
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-06-30 19:38:26 -0300

    feat(app/home): add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: 'Blog: Template using Next.js and Ghost CMS',
+  description: 'Exemple of blog using Next.js and Ghost CMS'
+}
+
 export default async function HomePage () {
   return (
     <main>
-      
+
     </main>
   )
 }
