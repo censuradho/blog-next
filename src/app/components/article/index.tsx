4ad95faaa8f22683cf0f68aff7254601c354f07c
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-07-01 10:30:35 -0300

    refactor(article): change styles

--- src/app/components/article/index.tsx
+++ src/app/components/article/index.tsx
@@ -3,6 +3,8 @@ import { resolvePath } from 'utils/helpers'
 
 import { Tag } from '@/app/components'
 
+import { format, formatDistance } from '@/lib/date-fns'
+import Image from 'next/image'
 import styles from './styles.module.css'
 import { ArticleProps } from './types'
 
@@ -17,9 +19,29 @@ export function Article ({ data }: ArticleProps) {
     </Tag>
   ))
 
+  const [author] =  data?.authors || []
+
+
+
+  const createdAt = format(new Date(data.created_at as string), 'MMM, yyy')
+  const dateDistance = formatDistance(new Date(data.created_at as string), new Date())
+
   return (
     <article className={styles.article}>
-      <span className={styles.article__created_at}>Jun 29 (2 days ago)</span>
+      <div className={styles.article__author}>
+        <Image 
+          className={styles.article__author__avatar}
+          src={author?.profile_image || ''}
+          alt={author?.name || ''}
+          width={25}
+          height={25}
+        />
+        <strong className={styles.article__author__title}>{author?.name}</strong>
+      </div>
+      <div>
+        <span className={styles.article__created_at}>{`${createdAt}`}</span>
+        <span className={styles['article__created_at--distance']}>{` (${dateDistance})`}</span>
+      </div>
       <h2 className={styles.article__title}>{data.title}</h2>
       {renderTags}
     </article>
--- src/lib/date-fns.ts
+++ src/lib/date-fns.ts
@@ -1,9 +1,18 @@
-import { format as dateFormat } from 'date-fns'
+import { 
+  format as dateFormat,
+  formatDistance as dateFormatDistance 
+} from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 
 
 export function format (date: number | Date, format: string) {
   return dateFormat(date, format, {
+    locale: ptBR
+  })
+}
+
+export function formatDistance (date: number | Date, baseDate: number | Date) {
+  return dateFormatDistance(date, baseDate, {
     locale: ptBR
   })
 }
