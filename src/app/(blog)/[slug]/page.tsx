7dd8671d6a92e64a626a1a872c26303cf4adfbfd
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-07-01 15:09:27 -0300

    feat(post page): add metadata

--- src/app/(blog)/[slug]/page.tsx
+++ src/app/(blog)/[slug]/page.tsx
@@ -1,5 +1,9 @@
-import { getPosts } from "lib/ghost";
+import { getPost, getPosts } from "lib/ghost";
 import { Params } from "../types";
+import { appSettings } from "@/config/app";
+import { Metadata } from "next";
+import { resolvePath } from "utils/helpers";
+import { paths } from "constants/routes";
 
 export async function generateStaticParams () {
   const posts = await getPosts()
@@ -7,6 +11,47 @@ export async function generateStaticParams () {
   return posts.map(value => ({
     slug: value.slug
   }))
+}
+
+export async function generateMetadata ({ params }: Params<{ slug: string }>): Promise<Metadata> {
+  const { slug } = params
+
+ 
+  const data = await getPost(slug, {
+    include: [
+      'authors',
+      "tags"
+    ]
+  })
+
+  const title =  data.meta_title || data.title
+  const description = data.meta_description || data.excerpt
+  const authors = data?.authors?.map(value => ({
+    name: value.name,
+  }))
+
+  const url = `${appSettings.siteUrl}/${resolvePath(paths.post, { slug })}`
+
+  return {
+    title,
+    description,
+    authors,
+    openGraph: {
+      title,
+      description,
+      authors: authors?.[0].name,
+      releaseDate: data.created_at,
+      modifiedTime: data.updated_at as string,
+      tags:  data.tags?.map(value => value.name as string),
+      url,
+    },
+    twitter: {
+      card: 'summary',
+      site: url,
+      title,
+      description
+    }
+  }
 }
 
 export const revalidate = 60
--- src/config/app.ts
+++ src/config/app.ts
@@ -1,4 +1,7 @@
 export const appSettings = {
+  appName: process.env.NEXT_PUBLIC_APP_NAME || '@procergs-concurso-2023',
+  siteUrl: process.env.NEXT_PUBLIC_API_URL || (process.env.VERCEL_URL && process.env.VERCEL_URL) || process.env.URL || 'http://localhost:3000',
+  analytics: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
   ghost: {
     url: process.env.GHOST_URL || '',
     key: process.env.GHOST_KEY || '',
