b7c430ab72da8fcda861b0004e0bec39e4040b73
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-07-01 10:05:37 -0300

    feat(article): add created at

--- src/app/components/article/index.tsx
+++ src/app/components/article/index.tsx
@@ -1,6 +1,8 @@
 import { paths } from 'constants/routes'
 import { resolvePath } from 'utils/helpers'
-import { Tag } from '../tag'
+
+import { Tag } from '@/app/components'
+
 import styles from './styles.module.css'
 import { ArticleProps } from './types'
 
@@ -17,7 +19,8 @@ export function Article ({ data }: ArticleProps) {
 
   return (
     <article className={styles.article}>
-      <h2>{data.title}</h2>
+      <span className={styles.article__created_at}>Jun 29 (2 days ago)</span>
+      <h2 className={styles.article__title}>{data.title}</h2>
       {renderTags}
     </article>
   )
