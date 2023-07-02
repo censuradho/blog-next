c84d856c4003d220e745c51c7937bb7249b7ef4e
censuradho <gustavoleiteoliveira800@gmail.com>
2023-07-02 00:53:33 -0300

    refactor(constants/legacy): remove icon prop

--- constants/routes.ts
+++ constants/routes.ts
@@ -1,7 +1,5 @@
-import { IconNames } from 'components/Icon'
 
 interface RoutePaths {
-  icon: IconNames,
   path: string,
   label: string
 }
@@ -12,7 +10,6 @@ export const routePaths: Record<Paths, RoutePaths> = {
   home: {
     path: '/',
     label: 'Blog',
-    icon: 'homeLine'
   }
 }
 
